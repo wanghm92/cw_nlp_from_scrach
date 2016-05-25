dacef21b5bf43a03020cae87f0f641a34fc19bb2
wanghm92 <wanghongminwhm@gmail.com>
2016-05-25 15:01:15 +0800

    before capitalization feature

---
+++ train.py
@@ -0,0 +1,209 @@
+import os
+from os import path
+import sys
+import nltk
+import time
+import argparse
+import itertools
+import xlwt
+from xlrd import open_workbook
+from xlutils.copy import copy
+import numpy as np
+import theano as theano
+import theano.tensor as T
+from theano import printing
+
+from utils import *
+from ffn import *
+
+_EMBEDDING_DIM = 100
+_HIDDEN_DIM = 360
+_LABEL_DIM = 45
+
+def contextWin(data_set, winsz):
+    assert (winsz % 2) == 1
+    assert winsz >= 1
+    ngrams = list()
+
+    for line in data_set:
+        ngrams_line = [line[i:(i+winsz)] for i in range(len(line)-winsz+1)]
+        assert len(ngrams_line) == len(line)-winsz+1
+        ngrams+=ngrams_line
+
+    return ngrams
+
+def test_with_trained_model(model, data, label, epoch, print_progress=False, load_model=False):
+    print print_progress
+
+    if load_model:
+        load_model_parameters_theano(model_para_output_file+"_%depochs.npz"%epoch, model)
+    
+    counter = 0
+    total = len(data)
+    tic = time.time()
+    
+    result = list()
+
+    for s in data:
+        counter += 1
+
+        result.append(model.predict([s])[0]+1)
+
+        if print_progress:
+            print '[testing] example %i >> %2.2f%%'%(counter,(counter+1)*100./total),'completed in %.2f (sec) <<\r'%(time.time()-tic),
+            sys.stdout.flush()
+
+    correct = [i for i, j in zip(result, label) if i == j]
+
+    acc = len(correct)*100./total
+
+    return acc
+
+def main():
+
+    ###################
+    # Section 0 Setup #
+    ###################
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--lr', type=float, default=0.01, action='store')
+    parser.add_argument('--winsz', type=int, default=5, action='store')
+    parser.add_argument('--epoch', type=int, default=20, action='store')
+    parser.add_argument('--batchsize', type=int, default=50, action='store')
+    parser.add_argument('--test_epoch', type=int, action='store')
+    parser.add_argument('--cont_epoch', type=int, action='store')
+    parser.add_argument('--print_loss', action='store_true')
+    parser.add_argument('--train', action='store_true')
+    parser.add_argument('--test', action='store_true')
+    parser.add_argument('--dev', action='store_true')
+    parser.add_argument('--iscontinue', action='store_true')
+    parser.add_argument('--print_progress', action='store_true')
+    parser.add_argument('--write_to_existing_excel', action='store_true')
+
+    args = parser.parse_args()
+
+    lr = np.float32(args.lr).astype(theano.config.floatX)
+    winsz = args.winsz
+    batch_size = args.batchsize
+    test_epoch = args.test_epoch
+    cont_epoch = args.cont_epoch
+    train = args.train
+    test = args.test
+    dev = args.dev
+    iscontinue = args.iscontinue
+    print_progress = args.print_progress
+    write_to_existing_excel = args.write_to_existing_excel
+
+    if write_to_existing_excel:
+        rb = open_workbook(excel_filename)
+        wb = copy(rb)
+        ws = wb.get_sheet(0)
+    else:
+        wb = xlwt.Workbook()
+        ws = wb.add_sheet('sheet')
+        excelSetup(ws)
+
+    #######################
+    # Section 1 Read Data #
+    #######################
+
+    train_set,train_label,test_set,test_label,dev_set,dev_label,input_vocab,output_vocab = readData(winsz)
+
+    vocab_size = len(input_vocab)
+    output_vocab_size = len(output_vocab)
+
+    train_ngrams = contextWin(train_set, winsz)
+    test_ngrams = contextWin(test_set,winsz)
+    dev_ngrams = contextWin(dev_set,winsz)
+
+    train_label = [i for sub in train_label for i in sub]
+    test_label = [i for sub in test_label for i in sub]
+    dev_label = [i for sub in dev_label for i in sub]
+
+    assert len(train_ngrams) == len(train_label)
+    assert len(test_ngrams) == len(test_label)
+    assert len(dev_ngrams) == len(dev_label)
+
+    ####################################
+    # Section 2 Initialize Model Graph #
+    ####################################
+
+    num_iter = len(train_ngrams) // batch_size + 1
+    print "num_iter = %d" % num_iter
+    
+    model = FFNTheano(winsz, vocab_size)
+
+    #########################
+    # Section 3 Train Model #
+    #########################
+
+    if iscontinue:
+
+        load_model_parameters_theano(model_para_output_file+"_%depochs.npz"%cont_epoch, model)
+
+    if train:
+
+        start_epoch = 1 if not iscontinue else cont_epoch+1
+
+        for i in range(start_epoch, args.epoch+1):
+
+            print "Training Epoch %d ......" %i
+            total_loss = 0
+            tic = time.time()
+
+            for j in range(num_iter):
+
+                if j == num_iter-1:
+                    ngram = np.asarray(train_ngrams[j*batch_size:])
+                    label = np.eye(_LABEL_DIM)[(np.array(train_label[j*batch_size:])- 1).tolist()]
+                else:
+                    ngram = np.asarray(train_ngrams[j*batch_size:(j+1)*batch_size])
+                    label = np.eye(_LABEL_DIM)[(np.array(train_label[j*batch_size:(j+1)*batch_size])- 1).tolist()]
+
+                total_loss += model.gradient_step(ngram, label, lr)            
+
+                if print_progress:
+                    print '[learning] epoch %i >> %2.2f%%'%(i,(j+1)*100./num_iter),'completed in %.2f (sec) <<\r'%(time.time()-tic),
+                    sys.stderr.flush()
+
+            print "Loss after epoch %i: %f" %(i, total_loss)
+
+            if dev:
+                print "Testing on dev set for Epoch %d ......" %i
+                accuracy = test_with_trained_model(model, dev_ngrams, dev_label, i, print_progress, False)
+                print "Dev accuracy after %d epochs of training : %2.2f%%" %(i, accuracy)
+                ws.write(i,0,accuracy)
+
+            if i%5 == 0:
+                print "Testing on training set for Epoch %d ......" %i
+                accuracy = test_with_trained_model(model, train_ngrams, train_label, i, print_progress,False)
+                print "Training accuracy after %d epochs of training : %2.2f%%" %(i, accuracy)
+                ws.write(i,1,accuracy)
+
+            save_model_parameters_theano(model_para_output_file+"_%depochs"%i, model)
+            print "Model after %d epochs is saved to %s" %(i, model_para_output_file+"_%depochs.npz"%i)
+
+            print "Training completed for %d epochs in %.2f seconds" %(i, time.time() -tic)
+
+            wb.save(excel_filename)
+
+    ########################
+    # Section 4 Test Model #
+    ########################
+
+    elif test:
+
+        accuracy = test_with_trained_model(model, test_ngrams, test_label, test_epoch, print_progress,True)
+
+        ws.write(test_epoch-1,2,accuracy)
+        ws.write(0,3,"Test Epoch")
+        ws.write(test_epoch-1,3,test_epoch)
+
+        print "Accuracy = %2.2f%%" %accuracy
+
+        wb.save(excel_filename)
+
+    wb.save(excel_filename)    
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,214 @@
+import numpy as np
+import theano as theano
+import theano.tensor as T
+import os, re
+from os import path
+import operator
+import xlwt
+
+###################
+# Section 0 Setup #
+###################
+
+data_dir = path.abspath('/home/shamil/Workspace/lm/corelm_Mar2016/_data')
+
+data_path = path.join(data_dir,'pos')
+model_path = path.abspath('../model')
+output_path = path.abspath('../output')
+
+train_file = path.join(data_path,'train/train.conll.txt')
+train_feat_file = path.join(data_path,'train.dat')
+train_label_file = path.join(data_path,'train.label.dat')
+
+test_file = path.join(data_path,'test/test.conll.txt')
+test_feat_file = path.join(data_path,'test.dat')
+test_label_file = path.join(data_path,'test.label.dat')
+
+dev_file = path.join(data_path,'dev/dev.conll.txt')
+dev_feat_file = path.join(data_path,'dev.dat')
+dev_label_file = path.join(data_path,'dev.label.dat')
+
+input_vocab_file = path.join(data_path,'input.vocab')
+input_feat_vocab_file = path.join(data_path,'input.features.vocab')
+output_vocab_file = path.join(data_path,'output.vocab')
+
+model_para_output_file = path.join(model_path,'model')
+# model_para_input_file = path.join(model_path, 'model.npz')
+excel_filename = path.join(output_path,'stats.xls')
+
+_PADDING = '<s>'
+_UNKNOWN = '<unk>'
+
+def excelSetup (ws):
+
+    ws.write(0,0,"Dev Acc")
+    ws.write(0,1,"Train Acc")
+    ws.write(0,2,"Test Acc")
+
+#######################
+# Section 1 Read Data #
+#######################
+
+def readData(winsz):
+
+    def readDataSet(winsz,file):
+
+        data_set = list()
+
+        with open(file) as f:
+            for line in f:
+                padded_line = [re.sub(r'(-?\d+)','NUMBER',a) for a in line.lower().strip().split()]
+                for i in range(winsz/2):
+                    padded_line.insert(0,_PADDING)
+                    padded_line.append(_PADDING)
+                data_set.append(padded_line)
+
+        return data_set
+
+    def readLabel(file):
+        
+        label = list()
+        
+        with open(file) as f:
+            for line in f:
+                label.append(line.strip().split())
+
+        return label
+
+    def readVocab(file,isInput):
+
+        vocab = list()
+        vocab_dict = dict()
+        i = 0
+        with open(file) as f:  
+            for line in f:
+                i+=1
+                if isInput:
+                    line = line.strip()
+                else:
+                    line = line.strip().split()[0]
+
+                vocab.append(line)
+                vocab_dict[line] = i
+
+        return vocab, vocab_dict
+
+    input_vocab, input_vocab_dict = readVocab(input_vocab_file,True)
+    output_vocab, output_vocab_dict = readVocab(output_vocab_file,False)
+
+    input_vocab_index = range(1,len(input_vocab)+1)
+    output_vocab_index = range(1,len(output_vocab)+1)
+
+    train_set = readDataSet(winsz,train_file)
+    train_index = [[input_vocab_dict[w] if w in input_vocab_dict else input_vocab_dict[_UNKNOWN] for w in t] for t in train_set]
+    train_label = readLabel(train_label_file)
+    train_label_index = [[output_vocab.index(x)+1 for x in l] for l in train_label]
+
+    test_set = readDataSet(winsz,test_file)
+    test_index = [[input_vocab_dict[w] if w in input_vocab_dict else input_vocab_dict[_UNKNOWN] for w in t] for t in test_set]
+    test_label = readLabel(test_label_file)
+    test_label_index = [[output_vocab.index(x)+1 for x in l] for l in test_label]
+
+    dev_set = readDataSet(winsz,dev_file)
+    dev_index = [[input_vocab_dict[w] if w in input_vocab_dict else input_vocab_dict[_UNKNOWN] for w in t] for t in dev_set]
+    dev_label = readLabel(dev_label_file)
+    dev_label_index = [[output_vocab.index(x)+1 for x in l] for l in dev_label]
+
+    return train_index,train_label_index,test_index,test_label_index,dev_index,dev_label_index,input_vocab_index,output_vocab_index
+
+########################
+# Section 2 Save Model #
+########################
+
+def save_model_parameters_theano(outfile, model):
+
+    if not os.path.exists(model_path):
+        os.makedirs(model_path)
+
+    window_size, word_dim, embedding_dim, hidden_dim, input_dim, label_dim, lt, w1, b1, w2, b2 = model.window_size, model.word_dim, model.embedding_dim, model.hidden_dim, model.input_dim, model.label_dim, model.lt.get_value(), model.w1.get_value(), model.b1.get_value(), model.w2.get_value(), model.b2.get_value()
+    np.savez(outfile, window_size=window_size, word_dim=word_dim, embedding_dim=embedding_dim, hidden_dim=hidden_dim, input_dim=input_dim, label_dim=label_dim, lt=lt, w1=w1, b1=b1, w2=w2, b2=b2)
+    print "Saved model parameters to %s." % outfile
+
+########################
+# Section 3 Load Model #
+########################
+
+def load_model_parameters_theano(path, model):
+    npzfile = np.load(path)
+
+    window_size = npzfile["window_size"]
+    word_dim = npzfile["word_dim"]
+    embedding_dim = npzfile["embedding_dim"]
+    hidden_dim = npzfile["hidden_dim"]
+    input_dim = npzfile["input_dim"]
+    label_dim = npzfile["label_dim"]
+    lt = npzfile["lt"]
+    w1 = npzfile["w1"]
+    b1 = npzfile["b1"]
+    w2 = npzfile["w2"]
+    b2 = npzfile["b2"]
+
+    model.window_size = window_size
+    model.word_dim = word_dim
+    model.embedding_dim = embedding_dim
+    model.hidden_dim = hidden_dim
+    model.input_dim = input_dim
+    model.label_dim = label_dim
+    model.lt.set_value(lt)
+    model.w1.set_value(w1)
+    model.b1.set_value(b1)
+    model.w2.set_value(w2)
+    model.b2.set_value(b2)
+
+    print "Loaded model parameters from\n \"%s\"\nwindow_size = %d, word_dim (vocab size) = %d, embedding_dim = %d,  hidden_dim = %d, input_dim = %d, label_dim = %d\n" % (path, window_size, word_dim, embedding_dim, hidden_dim, input_dim, label_dim), "Dimensions of layers: shape of lt = %s, w1 = %s, b1 = %s, w2 = %s, b2 = %s" % (lt.shape, w1.shape, b1.shape, w2.shape, b2.shape,)
+
+############################
+# Section 4 Gradient Check #
+############################
+def gradient_check_theano(model, model_param, x, y, h=0.0001, error_threshold=0.01):
+    # Overwrite the bptt attribute. We need to backpropagate all the way to get the correct gradient
+    # model.bptt_truncate = 1000
+    # Calculate the gradients using backprop
+    print 'x: ', x
+    print 'y: ', y
+
+    gradients = model.calculate_gradients(x, y)
+    # List of all parameters we want to chec.
+    # model_parameters = ['U', 'V', 'W']
+    # Gradient check for each parameter
+    for pidx, pname in enumerate(model_param):
+        # Get the actual parameter value from the mode, e.g. model.W
+        parameter_T = operator.attrgetter(pname)(model)
+        parameter = parameter_T.get_value()
+        print "Performing gradient check for parameter %s with size %d." % (pname, np.prod(parameter.shape))
+        # Iterate over each element of the parameter matrix, e.g. (0,0), (0,1), ...
+        it = np.nditer(parameter, flags=['multi_index'], op_flags=['readwrite'])
+        while not it.finished:
+            ix = it.multi_index
+            # Save the original value so we can reset it later
+            original_value = parameter[ix]
+            # Estimate the gradient using (f(x+h) - f(x-h))/(2*h)
+            parameter[ix] = original_value + h
+            parameter_T.set_value(parameter)
+            gradplus = model.calculate_loss(x,y)
+            parameter[ix] = original_value - h
+            parameter_T.set_value(parameter)
+            gradminus = model.calculate_loss(x,y)
+            estimated_gradient = (gradplus - gradminus)/(2*h)
+            parameter[ix] = original_value
+            parameter_T.set_value(parameter)
+            # The gradient for this parameter calculated using backpropagation
+            backprop_gradient = gradients[pidx][ix]
+            # calculate The relative error: (|x - y|/(|x| + |y|))
+            relative_error = np.abs(backprop_gradient - estimated_gradient)/(np.abs(backprop_gradient) + np.abs(estimated_gradient))
+            # If the error is to large fail the gradient check
+            if relative_error > error_threshold:
+                print "Gradient Check ERROR: parameter=%s ix=%s" % (pname, ix)
+                print "+h Loss: %f" % gradplus
+                print "-h Loss: %f" % gradminus
+                print "Estimated_gradient: %f" % estimated_gradient
+                print "Backpropagation gradient: ", backprop_gradient
+                print "Relative Error: %f" % relative_error
+                return 
+            it.iternext()
+        print "Gradient check for parameter %s passed." % (pname)
